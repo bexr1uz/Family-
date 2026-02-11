4ca7dce2fdc3380fb5974156d3b95f6fae8c1cc9
Bexruz <bexruzbekdudiyev@gmail.com>
2026-02-11 13:06:37 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,31 +1,79 @@
-let prices = [18300000, 12350000];
-let qty = [1, 1];
-let count = 0;
-let total = 0;
+let lists = [
+    {name: "Bakdurdi Matyoqubov", father: null},
+    {name: "Faxriddin Matyoqubov", father: "Bakdurdi Matyoqubov"},
+    {name: "Bexruz Bekdurdiyev", father: "Faxriddin Matyoqubov"},
+];
 
-function plus(i) {
-  qty[i]++;
-  document.getElementById("q" + i).innerText = qty[i];
+function deletePerson(name) {
+    lists = lists.filter(p => p.name !== name && p.father !== name);
+    renderTree();
 }
 
-function minus(i) {
-  if (qty[i] > 1) {
-    qty[i]--;
-    document.getElementById("q" + i).innerText = qty[i];
-  }
+function renderTree() {
+    const container = document.getElementById("tree");
+    container.innerHTML = "";
+    treeLists(null, container);
 }
 
-function add(i) {
-  count += qty[i];
-  total += prices[i] * qty[i];
-  document.getElementById("count").innerText = count;
-  document.getElementById("total").innerText = total.toLocaleString();
+function treeLists(father, container) {
+    lists
+        .filter(item => item.father === father)
+        .forEach(person => {
+            let node = document.createElement("div");
+            node.className = "node";
+
+            node.innerHTML = `
+                <div class="person">
+                    ${person.name}
+                    <button class="delBtn" onclick="deletePerson('${person.name}')">x</button>
+                </div>
+            `;
+
+            container.appendChild(node);
+            treeLists(person.name, node);
+        });
 }
 
+function addElement() {
+    let name = document.getElementById("childName").value.trim();
+    let father = document.getElementById("parentName").value.trim();
 
-function clearCart() {
-  count = 0;
-  total = 0;
-  document.getElementById("count").innerText = 0;
-  document.getElementById("total").innerText = 0;
+    if (!name) {
+        alert("Ismni kiriting!");
+        return;
+    }
+
+    lists.push({
+        name: name,
+        father: father || null
+    });
+
+    document.getElementById("childName").value = "";
+    document.getElementById("parentName").value = "";
+
+    renderTree();
 }
+
+function searchPerson() {
+    let search = document.getElementById("searchName").value.trim();
+
+    if (!search) {
+        alert("Ism kiriting!");
+        return;
+    }
+
+    const names = lists.map(person => person.name);
+    let position = names.indexOf(search);
+
+    if (position !== -1) {
+        document.getElementById("result").innerText =
+            search + " " + (position + 1) + "-o‘rinda joylashgan";
+    } else {
+        document.getElementById("result").innerText =
+            "Bunday ism topilmadi!";
+    }
+
+    document.getElementById("searchName").value = "";
+}
+
+renderTree();
